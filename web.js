22ad0f77e58616052361923329e2a0d673bd1d44
singh-bk <singh.bk@gmail.com>
2014-03-17 00:52:27 +0530

    init

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Welcome BK');
+  res.send('Hello World!');
 });
 
 var port = Number(process.env.PORT || 5000);
