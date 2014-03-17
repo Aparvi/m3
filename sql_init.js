e7bfd81e63eb221b9c25f6e8781a8c1286756033
singh-bk <singh.bk@gmail.com>
2014-03-18 00:44:55 +0530

    New

--- sql_init.js
+++ sql_init.js
@@ -4,7 +4,7 @@ var pg = require("pg");
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   if(client != null){
   console.log("client is not null");
-  var query = "CREATE TABLE [if not exists] DISH(dish_id bigserial primary_key, dish_name varchar(40) not null)";
+  var query = "CREATE TABLE if not exists DISH(dish_id bigserial primary_key, dish_name varchar(40) not null)";
   client.query(query, function(err, result) {
     done();
     if(err) {
