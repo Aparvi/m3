b4c59cbc776990b17b406f3f04fbd8e40ed9eaa7
singh-bk <singh.bk@gmail.com>
2014-03-17 23:47:03 +0530

    New Commit

--- web.js
+++ web.js
@@ -2,6 +2,22 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+var pg = require("pg");
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  if(client != null){
+  console.log("client is not null");
+  client.query('SELECT * FROM your_table', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+}else
+{
+	console.log("client is null");
+}
+});
+
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
