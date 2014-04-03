39335fc18d551d72f576d994fbd3a80c25011513
singh-bk <singh.bk@gmail.com>
2014-04-04 01:00:14 +0530

    worker modified to post

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 var app = require("express");
 
-app.get('/dish_image/:dish_id', function(req,res){
+app.post('/dish_image/:dish_id', function(req,res){
 	console.log("Inside the get method of worker process");
 });
