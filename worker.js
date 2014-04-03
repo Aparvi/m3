4162d1d083050049a173b711daf690b8a8edc90d
singh-bk <singh.bk@gmail.com>
2014-04-04 01:08:44 +0530

    worker modified to add port

--- worker.js
+++ worker.js
@@ -1,5 +1,12 @@
-var app = require("express");
+var express = require("express");
+var app = express();
 
 app.post('/dish_image/:dish_id', function(req,res){
 	console.log("Inside the get method of worker process");
+});
+
+//declare the port and listen to it
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
